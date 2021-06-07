9d7e236a214e77a13f5e023efaeb71ebc2c52e16
Your Name <lisseth.arce96@gmail.com>
2021-06-06 20:45:37 -0600

    tarea cuatro

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+function toggleBloque() {
+    
+
+    var demo= document.getElementById("operacionesContenedor");
+
+
+if (demo.style.display=== "none") {
+    demo.style.display= "block";
+    document.getElementById("toggleBloque").textContent = "Ocultar operaciones";
+
+} else {
+   
+    demo.style.display= "none";
+    document.getElementById("toggleBloque").textContent= "Mostrar operaciones";
+}
+
+
+}
+    
+function sumar() {
+
+    var resultado = parseInt(document.getElementById("myNumber1").value)+parseInt(document.getElementById("myNumber2").value);
+    function validarVacios(resultado,operacion) {
+            if(!isNaN(resultado)) {
+                imprimirMensaje(operacion,resultado);
+            } else {
+                document.getElementById('result').textContent = "Debe ingresar valores numericos";
+            }
+        }
+        
+        
+     }
+
+            
+            
+         
+            
+        
+        
+        
+     
+     
+    
+
+
+
